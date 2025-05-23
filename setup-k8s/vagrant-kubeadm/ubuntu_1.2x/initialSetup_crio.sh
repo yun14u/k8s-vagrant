2d6ec12c027bd66af211910bc54a9559f9dc81ac
yun14u <yun14u@yahoo.com>
2025-05-23 13:41:53 -0500

    Revamp initialSetup_crio.sh to accomodate k8s 1.3x

--- setup-k8s/vagrant-kubeadm/ubuntu_1.2x/initialSetup_crio.sh
+++ setup-k8s/vagrant-kubeadm/ubuntu_1.2x/initialSetup_crio.sh
@@ -11,49 +11,63 @@ net.bridge.bridge-nf-call-ip6tables = 1
 EOF
 sysctl --system
 
-echo "[TASK 2] install crio"
+echo "[TASK 2] prep repo"
 export OS=xUbuntu_22.04
-export CRIO_VERSION=1.26
+export CRIO_VERSION=1.29
 
-echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /"| sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
-echo "deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$CRIO_VERSION/$OS/ /"| sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$CRIO_VERSION.list
+curl -fsSL https://download.opensuse.org/repositories/isv:/cri-o:/stable:/v${CRIO_VERSION}/deb/Release.key |gpg --dearmor | sudo tee /etc/apt/keyrings/cri-o-keyring.gpg > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/cri-o-keyring.gpg] https://download.opensuse.org/repositories/isv:/cri-o:/stable:/v${CRIO_VERSION}/deb/ ./" |  sudo tee /etc/apt/sources.list.d/cri-o.list
 
-curl -L https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$CRIO_VERSION/$OS/Release.key | sudo apt-key add -
-curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | sudo apt-key add -
+curl -fsSL https://download.opensuse.org/repositories/isv:/kubernetes:/core:/stable:/v${CRIO_VERSION}/deb/Release.key |gpg --dearmor | sudo tee /etc/apt/keyrings/kubernetes.gpg > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes.gpg] https://download.opensuse.org/repositories/isv:/kubernetes:/core:/stable:/v${CRIO_VERSION}:/build/deb/ ./" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
+sudo apt update
+
+echo "[TASK 3] install crio"
 apt update
-apt install cri-o cri-o-runc -y
+apt install cri-o -y
+apt install runc conmon -y
 
-systemctl start crio
-systemctl enable crio
-systemctl status crio
+systemctl start cri-o.service
+systemctl enable cri-o.service
 
-echo "[TASK 3] Add CNI Plugin for crio"
-apt install containernetworking-plugins -y
+systemctl status cri-o.service --no-pager
+
+echo "[TASK 4] post-crio installation task"
+# 1. Create /etc/crio directory
+sudo mkdir -p /etc/crio
+
+# 2. Generate the crio.conf
+crio config | sudo tee /etc/crio/crio.conf > /dev/null
+
+# 3. Now you can cd into it
 cd /etc/crio
-sed -i.bak 's|^# conmon = ""|conmon = "/usr/bin/conmon"|' crio.conf
-sed -i.bak 's|^# network_dir|network_dir|' crio.conf
-sed -i.bak 's|^# plugin_dirs |plugin_dirs |' crio.conf
-sed -i.bak  's|^# [[:blank:]]"/opt/cni/bin/",|     "/opt/cni/bin/","/usr/lib/cni/", ]|' crio.conf
 
-rm -f /etc/cni/net.d/100-crio-bridge.conflist
-curl -fsSLo /etc/cni/net.d/11-crio-ipv4-bridge.conflist https://raw.githubusercontent.com/cri-o/cri-o/main/contrib/cni/11-crio-ipv4-bridge.conflist
-systemctl restart crio
-apt install cri-tools -y
+# 4. Fix the conmon and network settings
+sudo sed -i.bak 's|^# *conmon = ""|conmon = "/usr/bin/conmon"|' crio.conf
+sudo sed -i.bak 's|^# *network_dir = .*|network_dir = "/etc/cni/net.d"|' crio.conf
+sudo sed -i.bak 's|^# *plugin_dirs = .*|plugin_dirs = [ "/opt/cni/bin/", "/usr/lib/cni/" ]|' crio.conf
+
+# 5. Install CNI plugins
+sudo apt install -y containernetworking-plugins
+
+# 6. Replace the CNI default config
+sudo rm -f /etc/cni/net.d/100-crio-bridge.conflist
+sudo mkdir -p /etc/cni/net.d
+sudo curl -fsSLo /etc/cni/net.d/11-crio-ipv4-bridge.conflist https://raw.githubusercontent.com/cri-o/cri-o/main/contrib/cni/11-crio-ipv4-bridge.conflist
+
+# 7. Restart CRI-O
+sudo systemctl daemon-reexec
+sudo systemctl restart crio
+systemctl status cri-o.service --no-pager
 
 
-echo "[TASK 4] Installing dependencies"
+echo "[TASK 5] Installing dependencies"
 apt-get update
 apt-get install -y apt-transport-https ca-certificates curl gpg -y
 
-echo "[TASK 8] Add apt repo for kubernetes"
-#curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - >/dev/null 2>&1
-#apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main" >/dev/null 2>&1
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.27/deb/Release.key | sudo gpg --dearmor     -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-# This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.27/deb/ /' | tee /etc/apt/sources.list.d/kubernetes.list
 
-echo "[TASK 5] intall kubernetes apps"
+echo "[TASK 6] intall kubernetes apps"
 apt-get update
 apt-get install -y kubelet kubeadm kubectl
 apt-mark hold containerd kubelet kubeadm kubectl
--- setup-k8s/vagrant-kubeadm/ubuntu_1.2x/setupmaster.sh
+++ setup-k8s/vagrant-kubeadm/ubuntu_1.2x/setupmaster.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
-
+VERSION=1.29.15
 echo "[TASK 1] Pull required containers"
-kubeadm config images pull --kubernetes-version=1.27.10 >/dev/null 2>&1
+kubeadm config images pull --kubernetes-version=${VERSION} >/dev/null 2>&1
 
 echo "[TASK 2] Initialize Kubernetes Cluster"
-#kubeadm init --apiserver-advertise-address=172.16.16.100 --pod-network-cidr=192.168.0.0/16 >> /root/kubeinit.log 2>/dev/null
 #crio
-kubeadm init --pod-network-cidr 10.244.0.0/16 --apiserver-advertise-address=192.168.56.2 --kubernetes-version=1.27.10 --cri-socket unix:///var/run/crio/crio.sock  >> /root/kubeinit.log
+kubeadm init --pod-network-cidr 10.244.0.0/16 --apiserver-advertise-address=192.168.56.2 --kubernetes-version=${VERSION} --cri-socket unix:///var/run/crio/crio.sock  >> /root/kubeinit.log
 
 echo "[TASK 3] Setup kubectl"
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 
-echo "[TASK 4] Deploy Calico network"
-kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://docs.projectcalico.org/v3.25/manifests/calico.yaml >/dev/null 2>&1
-
 #echo "[TASK 4] Deploy Flannel network"
-#curl -o kube-flannel.yml https://raw.githubusercontent.com/coreos/flannel/v0.9.1/Documentation/kube-flannel.yml
-#sed -i.bak 's|"/opt/bin/flanneld",|"/opt/bin/flanneld", "--iface=enp0s8",|' kube-flannel.yml
-#kubectl create -f kube-flannel.yml
+kubectl create -f /var/tmp/flannel/kube-flannel.yml
 
 echo "[TASK 5] Generate and save cluster join command to /joincluster.sh"
 kubeadm token create --print-join-command > /joincluster.sh 2>/dev/null
