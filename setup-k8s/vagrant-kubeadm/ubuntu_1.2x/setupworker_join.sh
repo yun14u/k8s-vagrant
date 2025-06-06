675b5ffb3d469e60f60eb8b2b3399b2eb3b05a02
yun14u <yun14u@yahoo.com>
2025-06-06 08:51:10 -0500

    add default setupworker.sh [join]

---
+++ setup-k8s/vagrant-kubeadm/ubuntu_1.2x/setupworker_join.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "[TASK 1] Prepare SSH key to Kubernetes Cluster"
+cp /var/tmp/keys/id_ecdsa ~/.ssh/id_ecdsa
+chmod 400 ~/.ssh/id_ecdsa
+
+echo "[TASK 2] Get script to join Cluster"
+scp -v -i ~/.ssh/id_ecdsa -o StrictHostKeyChecking=no root@192.168.56.2:/joincluster.sh /joincluster.sh
+
+echo "[Task 4] Join the cluster"
+bash /joincluster.sh
+echo "@master: kubectl label node $(hostname -s)  node-role.kubernetes.io/worker=worker"
